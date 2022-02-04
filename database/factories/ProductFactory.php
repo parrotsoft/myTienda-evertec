b2f846afd4238da12f2409111cadec8bf13a4fa7
Miguel Lopez Ariza <lopezarizamiguel@gmail.com>
2022-02-03 23:22:33 -0500

    feat: adding factory and seeders to the product model

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->text(200),
+            'price' => $this->faker->numberBetween($min = 1000, $max = 100000),
+            'active' => true,
+            'photo' => $this->faker->imageUrl($width = 500, $height = 350)
+        ];
+    }
+}
