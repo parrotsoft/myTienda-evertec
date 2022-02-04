df0fc5b73265a216d3e3193af67b071c9cae06f4
Miguel Lopez Ariza <lopezarizamiguel@gmail.com>
2022-02-03 23:34:34 -0500

    feat: rendering the products that are stored

--- resources/views/components/product-item.blade.php
+++ resources/views/components/product-item.blade.php
@@ -1,19 +1,19 @@
 <section>
     <div class="card shadow-sm border-light mb-4">
         <a href="#" class="position-relative">
-            <img src="https://via.placeholder.com/500x350/5fa9f8/ffffff" class="card-img-top" alt="image">
+            <img src="{{ $product['photo'] }}" class="card-img-top" alt="image">
         </a>
         <div class="card-body">
             <a href="#">
-                <h5 class="font-weight-normal">{{ $product['product_name'] }}</h5>
+                <h5 class="font-weight-normal">{{ $product['name'] }}</h5>
             </a>
             <div class="post-meta"><span class="small lh-120">
-                    <i class="fas fa-map-marker-alt mr-2"></i>{{ $product['product_description'] }}</span>
+                    <i class="fas fa-map-marker-alt mr-2"></i>{{ $product['description'] }}</span>
             </div>
             <div class="d-flex justify-content-between">
                 <div class="col pl-0">
                     <span class="text-muted font-small d-block mb-2">Precio :</span>
-                    <span class="h5 text-dark font-weight-bold">$ {{ $product['producto_price'] }}</span>
+                    <span class="h5 text-dark font-weight-bold">$ {{ number_format($product['price'], 2) }}</span>
                 </div>
                 <div class="col pr-0">
                     <button class="btn btn-info btn-sm d-block mt-4 w-100">Comprar</button>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,13 @@
     <section class="pt-5 pb-5">
         <div class="container">
             <div class="row mb-md-2">
-                <div class="col-md-6 col-lg-4">
-                    @component("components.product-item", [ 'product' => [ 'product_name' => 'PC', 'product_description' => 'Lo mejor', 'producto_price' => 10000 ]])
+                @foreach($products as $products)
+                    <div class="col-md-6 col-lg-4">
+                        @component("components.product-item", [ 'product' => $products])
 
-                    @endcomponent
-                </div>
+                        @endcomponent
+                    </div>
+                @endforeach
             </div>
         </div>
     </section>
