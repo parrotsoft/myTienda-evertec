6bc5dc832128fb2c90c88c31ea56507b3b18efa4
Miguel Lopez Ariza <lopezarizamiguel@gmail.com>
2022-02-05 19:29:32 -0500

    feat: adding order details component

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -12,18 +12,8 @@
                 @endcomponent
             </div>
             <div class="col">
-                <div class="mb-2">
-                    <p class="mb-0 fw-bold text-secondary">Producto</p>
-                    <p class="mb-0">$85,977.00</p>
-                </div>
-                <div class="mb-2 border-bottom pb-2" style="border-bottom-width: 2px !important; border-bottom-style: dashed !important">
-                    <p class="mb-0 fw-bold text-secondary">Envío</p>
-                    <p class="mb-0 text-success">Gratis</p>
-                </div>
-                <div>
-                    <p class="mb-0 fw-bold text-secondary">Total</p>
-                    <p class="mb-0">$30,000</p>
-                </div>
+                @component("components.order-details", ['product' => $order['product']])
+                @endcomponent
             </div>
             <button class="btn btn-sm btn-primary fs-5">Pagar</button>
         </div>
